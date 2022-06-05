b9334d34ff208c140bda9e769bce71a7b6f0a01b
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-06-04 17:29:30 -0700

    gallery html and css

--- app.js
+++ app.js
@@ -148,5 +148,21 @@ currentProductSizes.forEach((size, index)=>{
 });
 
 
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener('click',()=>{
+    payment.style.display='flex'
+});
+
+
+close.addEventListener('click',()=>{
+    payment.style.display='none'
+});
+
+
+
+
 
 
